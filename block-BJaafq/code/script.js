b7d61222f686f05ccba5a8dceff50bf1e4cde3a4
fruitycigar <shruthisagar@SHODAN.local>
2021-12-15 18:11:06 +0530

    failed test, brings back memories

---
+++ block-BJaafq/code/script.js
@@ -0,0 +1,11 @@
+function addNum(numA = 0, numB = 0) {
+    return numA + numB;
+}
+
+function testAddOne() {
+    result = addNum(1, 2);
+    expected = 3;
+    if (typeof numA !== 'number' || typeof numB !== 'number') {
+        throw new Error(``)
+    }
+}
