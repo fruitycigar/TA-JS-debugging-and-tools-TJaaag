0a9ba1cc7da6b7710d83c5800fe05cd892e21cc5
fruitycigar <shruthisagar@SHODAN.local>
2021-12-16 13:19:41 +0530

    committing for posterity

--- block-BJaafq/code/script.js
+++ block-BJaafq/code/script.js
@@ -2,10 +2,64 @@ function addNum(numA = 0, numB = 0) {
     return numA + numB;
 }
 
-function testAddOne() {
-    result = addNum(1, 2);
-    expected = 3;
-    if (typeof numA !== 'number' || typeof numB !== 'number') {
-        throw new Error(``)
+let result, expected;
+
+function test(message, cb) {
+    try {
+        cb();
+        console.log('✅', message);
+    } catch (error) {
+        console.error(error);
+        console.log('❌', message);
     }
 }
+
+function testAddOne() {
+    result = addNum(true, 2);
+    expected = 'number';
+    if (typeof numA !== expected || typeof numB !== expected) {
+        throw new Error(`One of the arguments is not of the number data type.`)
+    }
+}
+
+test(`Adding true and 2 to get an error message.`, testAddOne);
+
+function testAddTwo() {
+    result = addNum(3, 2);
+    expected = 'number';
+    if (typeof numA !== expected || typeof numB !== expected) {
+        throw new Error(`One of the arguments is not of the number data type.`)
+    }
+}
+
+test(`Adding 3 and 2 to get number.`, testAddTwo);
+
+function testAddThree() {
+    result = addNum(8, 2);
+    expected = 'number';
+    if (typeof numA !== expected || typeof numB !== expected) {
+        throw new Error(`One of the arguments is not of the number data type.`)
+    }
+}
+
+test(`Adding 8 and 2 to get number.`, testAddThree);
+
+function testAddFour() {
+    result = addNum(1, 5);
+    expected = 'number';
+    if (typeof numA !== expected || typeof numB !== expected) {
+        throw new Error(`One of the arguments is not of the number data type.`)
+    }
+}
+
+test(`Adding 1 and 5 to get number.`, testAddFour);
+
+function testAddFive() {
+    result = addNum(2, 2);
+    expected = 'number';
+    if (typeof numA !== expected || typeof numB !== expected) {
+        throw new Error(`One of the arguments is not of the number data type.`)
+    }
+}
+
+test(`Adding 2 and 2 to get number.`, testAddFive);
